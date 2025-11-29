0902a81ccab7e7ad651a9cde97e0a3accc81d91c
Zcyde <148860314+Zcyde@users.noreply.github.com>
2025-11-29 18:13:37 +0800

    Add files via upload

---
+++ Prelim Lab Activities/Prelim Lab Angular Directives/app/data-binding/data-binding.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-data-binding',
+  imports: [FormsModule],
+  templateUrl: './data-binding.html',
+  styleUrl: './data-binding.css',
+})
+export class DataBinding {
+  message = "Data Binding Demonstration";
+  imageUrl = 'picko.png';
+  w = 300;
+  h = 300;
+  altText = '2x2 Pic ko';
+  textColor = 'red';
+  isHighlighted = true;
+  yourName ='';
+  count = 0;
+  increment() {
+    this.count++;
+  }
+  decrement() {
+    this.count--;
+  }
+
+  // Part 2
+  // Interpolation
+  studentName = "Eldrin Josh Pineda";
+  score = 95;
+
+  // Property Binding
+  imageUrl2 = "pickoulet.jpg"
+  isDisabled = true;
+
+  // Attribute binding
+  colSpanValue = 3;
+
+  // Class binding
+  isPassing = true;
+
+  // Style binding
+  boxColor = "purple";
+  boxSize = "150px";
+}
+
